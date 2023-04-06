ac93f5ebe1bb183b2ed6e2dde30c9f3237545338
EC2 Default User <ec2-user@ip-172-31-11-78.eu-west-1.compute.internal>
2023-04-06 04:50:31 +0000

    Add some files

---
+++ class2/main.tf
@@ -0,0 +1,47 @@
+resource "aws_instance" "web" {
+  ami           = "ami-06e46074ae430fba6" # us-east-1
+  instance_type = "t3.micro"
+  associate_public_ip_address = true
+  availability_zone = "us-east-1a"
+  key_name = aws_key_pair.class2.key_name
+
+}
+
+resource "aws_key_pair" "class2" {
+    key_name = "class2"
+    public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "class2" {
+  name        = "class2"
+  description = "Allow TLS inbound traffic"
+ 
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
