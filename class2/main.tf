360ef65f49d692080271d310ac0ccc008c92e86e
EC2 Default User <ec2-user@ip-172-31-11-78.eu-west-1.compute.internal>
2023-04-06 05:24:24 +0000

    add route 53

--- class2/main.tf
+++ class2/main.tf
@@ -1,29 +1,29 @@
 
 
 resource "aws_key_pair" "class2" {
-    key_name = "class2"
-    public_key = file("~/.ssh/id_rsa.pub")
+  key_name   = "class2"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
 
 resource "aws_security_group" "class2" {
   name        = "class2"
   description = "Allow TLS inbound traffic"
- 
+
 
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -42,10 +42,10 @@ ingress {
 
 
 resource "aws_ebs_volume" "class2" {
-    availability_zone = "us-east-1a"
-    size = 40
+  availability_zone = "us-east-1a"
+  size              = 40
 }
- 
+
 resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdb"
   volume_id   = aws_ebs_volume.class2.id
@@ -56,15 +56,27 @@ resource "aws_volume_attachment" "ebs_att" {
 
 
 resource "aws_instance" "web" {
-  ami           = "ami-06e46074ae430fba6" # us-east-1
-  instance_type = "t3.micro"
+  ami                         = "ami-06e46074ae430fba6" # us-east-1
+  instance_type               = "t3.micro"
   associate_public_ip_address = true
-  availability_zone = "us-east-1a"
-  key_name = aws_key_pair.class2.key_name
+  availability_zone           = "us-east-1a"
+  key_name                    = aws_key_pair.class2.key_name
   vpc_security_group_ids = [
     aws_security_group.class2.id,
-    ]
+  ]
 }
 
+
+resource "aws_route53_record" "class2" {
+  allow_overwrite = true
+  zone_id         = var.zone_id
+  name            = "blog.${var.domain}"
+  type            = "A"
+  ttl             = 300
+  records         = [aws_instance.web.public_ip]
+}
+
+variable "zone_id" {}
+variable "domain" {}
 
 
