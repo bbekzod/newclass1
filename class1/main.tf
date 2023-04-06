b4a07b4a1af4931014018379020a837c690615c1
EC2 Default User <ec2-user@ip-172-31-11-78.eu-west-1.compute.internal>
2023-04-06 02:14:34 +0000

    Update

--- class1/main.tf
+++ class1/main.tf
@@ -1,3 +1,45 @@
-resource "aws_iam_user" "firstuser" {
-    name = "firstuser"
+resource "aws_iam_user" "users" {
+  for_each = toset([
+    "firstuser",
+    "seconduser",
+    "thirduser",
+  ])
+  name = each.key
 }
+
+resource "aws_iam_group" "multigroup" {
+  for_each = toset([
+    "sales",
+    "devops",
+    "marketing",
+  ])
+  name = each.key
+}
+
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "first-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-bucet-bekzod"
+}
+
+resource "aws_iam_user" "tim" {
+    name = "tim"
+}
+
+resource "aws_iam_group" "environment" {
+    name = "environment"
+}
+
+resource "aws_iam_group_membership" "team" {
+    name = "tf-testing-group-membership"
+    users = [
+        aws_iam_user.tim.name
+    ]
+    group = aws_iam_group.environment.name
+}
+
+
