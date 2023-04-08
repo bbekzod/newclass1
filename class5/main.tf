075de591c34e244434d3f4e3fb1db71d42ca0d9b
EC2 Default User <ec2-user@ip-172-31-11-78.eu-west-1.compute.internal>
2023-04-08 06:04:33 +0000

    add variables

--- class5/main.tf
+++ class5/main.tf
@@ -1,4 +1,12 @@
 resource "aws_key_pair" "state-demo" {
     key_name = "state-demo-key"
     public_key = file("~/.ssh/id_rsa.pub")
+    tags = var.tags
 }
+
+resource "aws_key_pair" "state-demo2" {
+    key_name = "state-demo2-key"
+    public_key = file("~/.ssh/id_rsa.pub")
+    tags = var.tags
+}
+
---
+++ class5/variable.tf
@@ -0,0 +1,13 @@
+variable tags {
+    type = map
+    default = {
+        Name = "Demo"
+        Team = "Devops"
+        Quarter = 2
+    }
+}
+
+variable region {
+    
+    default = "us-east-2"
+}
